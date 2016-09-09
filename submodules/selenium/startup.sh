6e7527e0ccc6f00f8177553d40a466632b688c2a
pboethig <pboethig@gmail.com>
2016-09-10 00:03:22 +0200

    mod startup sccripts

--- submodules/selenium/startup.sh
+++ submodules/selenium/startup.sh
@@ -4,12 +4,25 @@ reset=`tput sgr0`
 yellow=`tput setaf 3`
 
 echo "${green}Starting selenium service${reset}"
-cd docker-selenium
-docker-compose up --build -d 
+cd elgalu-docker-selenium/
+docker-compose --file docker-compose-host.yml up --build -d 
+
+
+docker run -d --name=grid -p 4444:24444 -p 5900:25900 \
+    -e TZ="US/Pacific" -v /dev/shm:/dev/shm elgalu/selenium
+
+
+docker exec grid wait_all_done 30s
+
 
 echo  "${green}######################################################################"
-echo  "${green}#  You can reach nexus under : ${yellow} http://localhost:8083 ${green}now           #"
-echo  "${green}#  Windows / Mac Users can use: ${yellow} http://<docker-machine-ip>:8083 ${green}now #"
+echo  "${green}#  You can reach selenium gui under : ${yellow} http://localhost:4444/grid/console ${green}now"
+echo ""
+echo  "${green}#  You can reach selenium gui under : ${yellow} http://localhost:4444/grid/console ${green}now"
+
+echo "Selenium Hub Url: http://localhost:4444/wd/hub"
+echo "Selenium Hub Url: http://<docker-machine-ip>:4444/wd/hub"
+
+echo ""
+echo  "${green}#  Windows / Mac Users can use: ${yellow} http://<docker-machine-ip>:444/grid/console ${green}now #"
 echo  "${green}######################################################################${reset}"
-
-
