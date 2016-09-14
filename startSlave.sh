ee4158861936e1ca76eba9c718203402cd44dc0d
pboethig <pboethig@gmail.com>
2016-09-14 23:09:32 +0200

    add

--- startSlave.sh
+++
@@ -1,1 +0,0 @@
-nohup java -jar slave.jar -jnlpUrl http://dockergitlab_jenkins_1:8080/computer/dockergitlab_webserver_1/slave-agent.jnlp &
--- submodules/git/startSlave.sh
+++ submodules/git/startSlave.sh
@@ -1,1 +1,1 @@
-nohup java -jar slave.jar -jnlpUrl java -jar slave.jar -jnlpUrl http://dockergitlab_jenkins_1:8080/computer/dockergitlab_webserver_1/slave-agent.jnlp &
+nohup java -jar slave.jar -jnlpUrl http://dockergitlab_jenkins_1:8080/computer/dockergitlab_webserver_1/slave-agent.jnlp &
